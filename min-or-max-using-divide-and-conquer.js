68e3a803aa174892fa761aa77097bde9af68356f
laraprabhu <laraprabhu@gmail.com>
2021-08-08 13:18:36 +0530

    Update min-or-max-using-divide-and-conquer.js

--- min-or-max-using-divide-and-conquer.js
+++ min-or-max-using-divide-and-conquer.js
@@ -6,7 +6,7 @@ let x = [4, 135, 3];
 
 const findMax = (array, i = -1, j = -1) => {
   const length = (i == -1) ? array.length : (j - i + 1);
-	const reformatedI = (i == -1 ? 0 : i);
+  const reformatedI = (i == -1 ? 0 : i);
   const reformatedJ = (j == -1 ? array.length - 1: j);
 
   if (length == 1) return array[reformatedI];
